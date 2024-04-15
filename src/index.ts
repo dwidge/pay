a71029dcd5e7929e121a4977d95aaca299006511
dwidge <dwidge+gh@gmail.com>
2024-04-16 01:04:04 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./Pay.js";
 export * from "./stripe/StripePay.js";
+export * from "./stripe/StripeTypes.js";
 export * from "./payfast/PayfastPay.js";
+export * from "./payfast/PayfastTypes.js";
 export { currencyCodes } from "./utils/currencyCodes.js";
